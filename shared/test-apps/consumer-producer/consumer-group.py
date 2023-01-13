a07a3cf1183aad36fad8ab531295979f969c51e4
merev <101055426+merev@users.noreply.github.com>
2023-01-13 21:23:20 +0200

    Update consumer-group.py

--- shared/test-apps/consumer-producer/consumer-group.py
+++ shared/test-apps/consumer-producer/consumer-group.py
@@ -3,7 +3,7 @@ from kafka import KafkaConsumer
 print('Consumer started. Press Ctrl+C to stop.')
 
 try:
-    consumer = KafkaConsumer(bootstrap_servers=['kafka-1:9092','kafka-2:9092','kafka-3:9092'], group_id='mygroup')
+    consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'], group_id='mygroup')
     consumer.subscribe(['demo4'])
     for message in consumer:
         print(message)
